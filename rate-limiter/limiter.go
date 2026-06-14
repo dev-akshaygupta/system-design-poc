0ed71f0caff6ab27198855c4c677cc2a7ab61db7
Akshay Gupta <dev.akshaygupta@gmail.com>
2026-06-13 22:04:31 -0400

    update: mutex version of rate limiter

--- rate-limiter/limiter.go
+++ rate-limiter/limiter.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
 type RateLimiter struct {
 	counter atomic.Int64
+	limit   int64
+}
+
+type RateLimiterWithMutex struct {
+	mu      sync.Mutex
+	counter int64
 	limit   int64
 }
 
@@ -37,6 +44,43 @@ func (rl *RateLimiter) StartResetTicker(done <-chan struct{}) {
 		select {
 		case <-ticker.C:
 			rl.Reset()
+		case <-done:
+			return
+		}
+	}
+}
+
+func NewRateLimiterWithMutex(limit int64) *RateLimiterWithMutex {
+	ratelimiter := RateLimiterWithMutex{
+		limit: limit,
+	}
+	return &ratelimiter
+}
+
+func (rlm *RateLimiterWithMutex) AllowWithMutex() bool {
+	rlm.mu.Lock()
+	rlm.counter++
+	if rlm.counter <= rlm.limit {
+		return true
+	}
+	rlm.mu.Unlock()
+	return false
+}
+
+func (rlm *RateLimiterWithMutex) ResetWithMutex() {
+	rlm.mu.Lock()
+	defer rlm.mu.Unlock()
+	rlm.counter = 0
+}
+
+func (rlm *RateLimiterWithMutex) StartResetTickerWithMutex(done <-chan struct{}) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			rlm.ResetWithMutex()
 		case <-done:
 			return
 		}
--- rate-limiter/main.go
+++ rate-limiter/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// with atomic
 func main() {
 	rateLimiter := NewRateLimiter(10)
 
@@ -37,3 +38,35 @@ func main() {
 	close(done)
 	wg.Wait()
 }
+
+// with mutex
+// func main() {
+// 	rateLimiterWithMU := NewRateLimiterWithMutex(10)
+
+// 	var wg sync.WaitGroup
+// 	done := make(chan struct{})
+// 	wg.Go(func() {
+// 		rateLimiterWithMU.StartResetTickerWithMutex(done)
+// 	})
+
+// 	for batch := range 2 {
+// 		var allowed int64
+// 		var denied int64
+
+// 		var bwg sync.WaitGroup
+// 		for range 25 {
+// 			bwg.Go(func() {
+// 				if rateLimiterWithMU.AllowWithMutex() {
+// 					atomic.AddInt64(&allowed, 1)
+// 				} else {
+// 					atomic.AddInt64(&denied, 1)
+// 				}
+// 			})
+// 		}
+// 		bwg.Wait()
+// 		fmt.Printf("Batch %d -> Allowed: %d, Denied: %d\n", batch, allowed, denied)
+// 		time.Sleep(1200 * time.Millisecond)
+// 	}
+// 	close(done)
+// 	wg.Wait()
+// }
